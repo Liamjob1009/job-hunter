5e764ee3932afa6fa5c7eb43e62801fed2073764
Liamjob1009 <liammia5@gmail.com>
2025-12-10 15:13:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -207,7 +207,7 @@ def main():
             
             history.append(job_id)
             
-           if job_data["score"] > 40 and telegram_token and telegram_chat_id:
+            if job_data["score"] > 40:
                 message = f"""🎮 <b>New Job Alert!</b>
 
 <b>Company:</b> {company_name}
